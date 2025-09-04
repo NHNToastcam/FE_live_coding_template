79166f85015a935f8149efddd8d82eada62c9f81
김형석 <seok7@nhn.com>
2025-09-04 16:49:45 +0900

    Modify : home 페이지 컴포넌트 기본으로 재설정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,7 @@
-import Modal from "@/components/Modal";
-import { useState } from "react";
-
 export function HomePage() {
-  const [isOpen, setIsOpen] = useState(false);
-
   return (
     <div>
       <h1>Home</h1>
-      <button onClick={() => setIsOpen((prev) => !prev)}>toggle</button>
-
-      <Modal isOpen={isOpen} onCloseModal={() => setIsOpen(false)}>
-        test
-      </Modal>
     </div>
   );
 }
