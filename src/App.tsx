1c4e3268b075cb46f09518634407fa13f09cb01c
김형석 <seok7@nhn.com>
2025-09-04 16:47:55 +0900

    Modify : 프로젝트 기본 상태로 되돌림.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.scss";
 import DefaultLayout from "./layout/DefaultLayout";
 import { HomePage } from "./pages/Home";
-import { UserDetailPage } from "./pages/UserDetail";
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <Routes>
         <Route path="/" element={<DefaultLayout />}>
           <Route index element={<HomePage />}></Route>
-          <Route path="/user/:id" element={<UserDetailPage />}></Route>
           <Route path="*" element={<div>Not Found</div>} />
         </Route>
       </Routes>
